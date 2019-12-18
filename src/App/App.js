ece79c7495d79662768323f0b891db8c1ef13d3c
Laura Collins <laura.e.collins1@gmail.com>
2019-12-17 21:27:59 -0600

    login a-go-go

--- src/App/App.js
+++ src/App/App.js
@@ -1,16 +1,41 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import firebaseConnection from '../helpers/data/connection';
 import NavBar from '../components/NavBar/NavBar';
 import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App"><NavBar />
-    <div className="authedContainer">
-      <img className="logo" src="https://user-images.githubusercontent.com/51382883/71045838-1981b600-20fc-11ea-99e6-268460213f63.png" />
+firebaseConnection();
+
+class App extends React.Component {
+  state = {
+    authed: false,
+  }
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
+  render () {
+    const { authed } = this.state;
+    return (
+      <div className="App"><NavBar authed={authed} />
+      <div className="authedContainer">
+        <img className="logo" src="https://user-images.githubusercontent.com/51382883/71045838-1981b600-20fc-11ea-99e6-268460213f63.png" />
+      </div>
     </div>
-  </div>
-  )
+    )
+  }
 }
 
 export default App;
