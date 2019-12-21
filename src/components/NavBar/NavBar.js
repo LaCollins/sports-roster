dc2525330e1513e123bb2b3f50862302e80750d8
Laura Collins <laura.e.collins1@gmail.com>
2019-12-21 09:55:35 -0600

    component printing

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -30,7 +30,7 @@ class NavBar extends React.Component {
     <div className="NavBar">
       <nav class="navbar navbar-dark bg-dark">
         <a class="navbar-brand" href="#">
-          <img src="https://user-images.githubusercontent.com/51382883/71045838-1981b600-20fc-11ea-99e6-268460213f63.png" alt="Mything Dungeons" />
+          <img src="https://user-images.githubusercontent.com/51382883/71045838-1981b600-20fc-11ea-99e6-268460213f63.png" alt="Mythic Dungeons" />
         </a>
         {this.buttonSwap()}
       </nav>
